c0b681fe9bac826b0ef6e2ead85633235f7eebb8
HoangNV <viethoangnhu@gmai.com>
2023-03-15 16:19:49 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Bé của anh',
+    introDesc: `Hãy tin anh người nhéeeee
+     Dẫu cho bao ngày sau như thế nào
+    
+    Thì em vẫn luôn là vợ của anh
+    🙃🙃🙃`,
+    btnIntro: '^^Oki chứ bé nhỉ^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Đúng là anh đẹp trai nhất đống đa thât cơ mà a vẫn muốn biết sao bé yêu anh <3',
+    btnReply: 'Gửi cho chồng <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 8h, anh qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100023082244262'
+}
